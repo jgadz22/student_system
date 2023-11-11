a8073709e8f4dd0daf718fda4875188d006f9a0d
Joseph <gadiazajoseph18@gmail.com>
2023-11-11 15:13:31 +0800

    designed the page

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ if ($connection->connect_error) {
 
 $sql = "SELECT * FROM student_list";
 $students = $connection->query($sql) or die($connection->error);
+$row = $students->fetch_assoc();
 
 ?>
 
@@ -23,6 +24,68 @@ $students = $connection->query($sql) or die($connection->error);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student System</title>
+
+    <style>
+        body {
+            font-family: "Arial";
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        table {
+            width: 100%;
+            text-align: center;
+            padding: 10px;
+            box-sizing: border-box;
+            background-color: #ffffff;
+            font-size: 16px;
+            border-collapse: collapse;
+            position: relative;
+            left: 0;
+        }
+
+        th,
+        td {
+            text-align: center;
+            padding: 15px;
+            box-sizing: border-box;
+        }
+
+        thead {
+            color: #ffffff;
+            font-weight: normal;
+            background-color: #8f8f8f;
+            border-bottom: solid 2px #d8d8d8;
+            position: sticky;
+            top: 0;
+        }
+
+        td {
+            border: solid 1px #d8d8d8;
+            border-left: 0;
+            border-right: 0;
+            white-space: nowrap;
+        }
+
+        h2 {
+            margin: 0;
+            padding: 0;
+        }
+
+        tbody>tr {
+            transition: background-color 150ms ease-out;
+
+            &:nth-child(2n) {
+                background-color: #f2f2f2;
+            }
+
+            &:hover {
+                background-color: #d0d0d0;
+            }
+        }
+    </style>
 </head>
 
 <body>
@@ -33,20 +96,24 @@ $students = $connection->query($sql) or die($connection->error);
     <table>
         <thead>
             <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
+                <th>
+                    <h2>First Name</h2>
+                </th>
+                <th>
+                    <h2>Last Name</h2>
+                </th>
             </tr>
         </thead>
         <tbody>
             <?php
-            while ($row = $students->fetch_assoc()) {
+            do {
             ?>
                 <tr>
                     <td><?php echo $row['first_name']; ?></td>
                     <td><?php echo $row['last_name']; ?></td>
                 </tr>
             <?php
-            }
+            } while ($row = $students->fetch_assoc())
             ?>
         </tbody>
     </table>
